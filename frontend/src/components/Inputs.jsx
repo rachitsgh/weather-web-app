efa98f73896ba199dd9f9342d5984a0dc1462b44
rachitsgh <rachit2351@gmail.com>
2024-07-28 23:26:42 +0530

    added input field for taking threshold value from user

--- frontend/src/components/Inputs.jsx
+++ frontend/src/components/Inputs.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { BiSearch, BiCurrentLocation } from "react-icons/bi";
 
-const Inputs = ({ setQuery, setUnits }) => {
+const Inputs = ({ setQuery, setUnits, setThreshold }) => {
   const [city, setCity] = useState("");
+  const [thre,setThre]=useState();
 
   const handleSearchClick = () => {
     if (city !== "") setQuery({ q: city });
   };
+
+  const handleThreSHold=()=>{
+    setThreshold(thre);
+  }
 
   const hanldeLocationClick = () => {
     if (navigator.geolocation) {
@@ -17,46 +22,63 @@ const Inputs = ({ setQuery, setUnits }) => {
     }
   };
 
+
+
   return (
-    <div className="flex flex-row justify-center my-6">
-      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+    <>
+      <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+          <input
+            value={city}
+            onChange={(e) => setCity(e.currentTarget.value)}
+            type="text"
+            placeholder="search by city..."
+            className="text-gray-500 text-xl font-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase"
+          />
+
+          <BiSearch
+            size={30}
+            className="cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleSearchClick}
+          />
+          <BiCurrentLocation
+            size={30}
+            className="cursor-pointer transition ease-out hover:scale-125"
+            onClick={hanldeLocationClick}
+          />
+        </div>
+
+        <div className="flex flex-row w-1/4 items-center justify-center">
+          <button
+            className="text-2xl font-medium transition ease-out hover:scale-125"
+            onClick={() => setUnits("metric")}
+          >
+            °C
+          </button>
+          <p className="text-2xl font-medium mx-1">|</p>
+
+          <button
+            className="text-2xl font-medium transition ease-out hover:scale-125"
+            onClick={() => setUnits("")}
+          >
+            °k
+          </button>
+
+        </div>
+      </div>
+      <div className="flex justify-center items-center\">
         <input
-          value={city}
-          onChange={(e) => setCity(e.currentTarget.value)}
+          value={thre}
+          onChange={(e) => setThre(e.currentTarget.value)}
           type="text"
-          placeholder="search by city..."
-          className="text-gray-500 text-xl font-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase"
+          placeholder="ThreShold temp..."
+          className="text-gray-500 text-xl font-light p-2 w-26 shadow-xl capitalize focus:outline-none placeholder:lowercase"
         />
-
-        <BiSearch
-          size={30}
-          className="cursor-pointer transition ease-out hover:scale-125"
-          onClick={handleSearchClick}
-        />
-        <BiCurrentLocation
-          size={30}
-          className="cursor-pointer transition ease-out hover:scale-125"
-          onClick={hanldeLocationClick}
-        />
-      </div>
-
-      <div className="flex flex-row w-1/4 items-center justify-center">
         <button
-          className="text-2xl font-medium transition ease-out hover:scale-125"
-          onClick={() => setUnits("metric")}
-        >
-          °C
-        </button>
-        <p className="text-2xl font-medium mx-1">|</p>
-
-        <button
-          className="text-2xl font-medium transition ease-out hover:scale-125"
-          onClick={() => setUnits("")}
-        >
-          °k
-        </button>
+          onClick={handleThreSHold}
+        className="mx-8 white text-black bg-white rounded-full w-40 h-10 transition ease-out   hover:scale-125"> set ThreShold </button>
       </div>
-    </div>
+    </>
   );
 };
 
